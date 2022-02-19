8f3d602f612954480b8c3be1bcc41cf3b24197aa
ranjeetk98 <53035334+ranjeetk98@users.noreply.github.com>
2022-02-19 13:20:08 +0530

    neglecting space and removing case sensitivity (#2)

---
+++ src/com/test/Anagram.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        System.out.println("Enter 1st string : ");
+        String s1 = inputString();
+        System.out.println("Enter 2nd string : ");
+        String s2 = inputString();
+
+        if(charCount(s1).equals(charCount(s2)))
+            System.out.println("'"+s1+"'" + " is ANAGRAM of "+ "'"+s2 +"'");
+        else
+            System.out.println("Not-Anagram");
+    }
+    public static String inputString(){
+        Scanner s = new Scanner(System.in);
+        return s.nextLine();
+    }
+
+    public static HashMap<Character,Integer> charCount(String s){
+        char[] ch = s.toUpperCase().replaceAll(" ","").toCharArray();
+        HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
+        for(char c : ch)
+            if(hm.containsKey(c))
+                hm.put(c,hm.get(c)+1);
+            else
+                hm.put(c,1);
+        return hm;
+    }
+
+
+}
