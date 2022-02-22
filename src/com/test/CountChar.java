92c1a9ae2780dbf23a1b7b3d32488def1df792e8
ranjeet <ranjeetk98@gmail.com>
2022-02-22 13:21:34 +0530

    cout char and reverse stirng

--- src/com/test/CountChar.java
+++ src/com/test/CountChar.java
@@ -1,31 +1,35 @@
 package com.test;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CountChar {
     public static void main(String[] args) {
+
         System.out.println("Enter a string : ");
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
-        // conver the string to upper case and remove all spaces
-        str = str.toUpperCase().replaceAll(" ","");
-        char[] ch = str.toCharArray();
-        HashMap<Character,Integer> hm = new HashMap<Character,Integer>();
-        for(char c : ch){
-            if(hm.containsKey(c))
-                hm.put(c,hm.get(c)+1);
-            else
-                hm.put(c,1);
-        }
+        HashMap<Character,Integer> hm = charCount(str);
+        System.out.println(hm);
 
-        for(Map.Entry<Character,Integer> e : hm.entrySet())
-        {
-            System.out.print(e.getKey() + ":"+ e.getValue()+", ");
-        }
-//        System.out.println(hm);
     }
+
+        static HashMap<Character,Integer> charCount(String str){
+
+            char[] ch = str.replaceAll(" ","").toLowerCase().toCharArray();
+            HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+            for (char c : ch) {
+                if (hm.containsKey(c))
+                    hm.put(c, hm.get(c) + 1);
+                else
+                    hm.put(c, 1);
+            }
+
+            return hm;
+
+        }
 
     
 
