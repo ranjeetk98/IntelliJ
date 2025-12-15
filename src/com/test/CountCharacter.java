6fa90fdbd47081bb4986fb777b7eca52b90ac39a
ranjeetk98 <53035334+ranjeetk98@users.noreply.github.com>
2025-12-15 21:25:47 +0530

    Addes StringConversion and CountCharacter files (#5)

---
+++ src/com/test/CountCharacter.java
@@ -0,0 +1,36 @@
+//Java program to count occurrence of each character in a string
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountCharacter {
+
+    public static void main(String args[]){
+
+        System.out.println("Enter a string : ");
+        Scanner scanner = new Scanner(System.in);
+        String s= scanner.nextLine();
+
+        countCharacterOccurence(s);
+    }
+
+    public static void countCharacterOccurence(String s){
+        HashMap<Character,Integer> hm = new HashMap<>();
+        s=s.toLowerCase();
+        s=s.replaceAll("\\s","");
+        System.out.println(s);
+
+        char[] ch = s.toCharArray();
+
+        for (char c : ch){
+
+            if(hm.get(c)==null)
+                hm.put(c,1);
+            else
+                hm.put(c,hm.get(c)+1);
+        }
+
+        System.out.print(hm);
+
+    }
+}
