54ab70505c327186251d5ab0727a04a8ba378aa6
vrukshata bhukya <ranjeetk98@gmail.com>
2025-12-15 21:19:10 +0530

    Addes StringConversion and CountCharacter files

---
+++ src/com/test/StringConversion.java
@@ -0,0 +1,22 @@
+package com.test;
+
+public class StringConversion {
+
+    public static void main(String[] args) {
+        String str = "tomorrow";
+        char[] c = str.toCharArray();
+        stringConvert(c);
+
+    }
+
+    public static void stringConvert(char[] c) {
+        for (int i=0; i<c.length; i++) {
+            if (c[i] == 'o')
+                c[i] = '3';
+            if(c[i]=='r')
+                c[i] ='2';
+        }
+
+        System.out.println(c);
+    }
+}
